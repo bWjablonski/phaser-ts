5a81af5352ca6707bc883c84a959fe152c0f0d14
bjablonski <bart@bjablonski.eu>
2022-09-30 19:16:40 +0200

    phaser init

---
+++ webpack.development.js
@@ -0,0 +1,21 @@
+const path = require('path');
+ 
+module.exports = {
+    entry: {
+        app: './src/main.js',
+    },
+    output: {
+        filename: 'main.js', 
+        path: path.resolve(__dirname, 'dist'),
+    },
+ 
+    mode: 'development',
+ 
+    devtool: 'inline-source-map',
+ 
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'src'),
+          },
+    }
+};
